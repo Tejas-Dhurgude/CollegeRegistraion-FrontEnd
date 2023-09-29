3fbbf0df1208074b684322b237dbd57520068d5d
Tejas-Dhurgude <116539440+Tejas-Dhurgude@users.noreply.github.com>
2023-09-29 15:47:08 +0530

    Update StudentRegister.js

--- src/pages/StudentRegister.js
+++ src/pages/StudentRegister.js
@@ -4,8 +4,8 @@ import Navbar from '../components/Navbar'
 const StudentRegister = () => {
   return (
     <>
-    <Navbar/>
-    <div className='flex flex-col justify-center'>
+    
+    {/* <div className='flex flex-col justify-center'>
         <div className='m-2'>
         <label className='text-xl font-semibold mr-2'>Student Name:</label>
         <input type="text" className='border-2' />
@@ -22,6 +22,32 @@ const StudentRegister = () => {
             <button className='bg-slate-400 text-lg p-2 rounded-lg hover:bg-slate-200'>Submit</button>
         </div>
     </div>
+     */}
+     <>
+    <div className='bg-img-institute h-screen bg-cover'>
+    <Navbar/>
+
+    <div className='flex flex-col justify-center bg-slate-200 max-w-fit rounded-lg m-32 p-8'>
+        <div className='m-4'>
+        <label className='text-xl font-semibold mr-2'>Student Name:</label>
+        <input type="text" className='border-2' />
+        </div>
+        <div className='m-4'>
+        <label className='text-xl font-semibold mr-2'>Student UID:</label>
+        <input type="text" className='border-2' />
+        </div>
+        <div className='m-4'>
+        <label className='text-xl font-semibold mr-2'>Wallet Address:</label>
+        <input type="text" className='border-2' />
+        </div>
+        <div className='m-4'>
+            <button className='bg-yellow-400 text-lg p-2 rounded-lg border-2 border-slate-400 hover:bg-yellow-200'>Submit</button>
+        </div>
+    </div>
+    </div>
+    
+    </>
+    
     </>
     
   )
