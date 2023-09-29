be43f931f84f5f047cf69660eb80a7655e275f04
Tejas-Dhurgude <116539440+Tejas-Dhurgude@users.noreply.github.com>
2023-09-29 15:48:27 +0530

    Update StudentRegister.js

--- src/pages/StudentRegister.js
+++ src/pages/StudentRegister.js
@@ -3,26 +3,8 @@ import Navbar from '../components/Navbar'
 
 const StudentRegister = () => {
   return (
-    <>
     
-    {/* <div className='flex flex-col justify-center'>
-        <div className='m-2'>
-        <label className='text-xl font-semibold mr-2'>Student Name:</label>
-        <input type="text" className='border-2' />
-        </div>
-        <div className='m-2'>
-        <label className='text-xl font-semibold mr-2'>Student EN number:</label>
-        <input type="text" className='border-2' />
-        </div>
-        <div className='m-2'>
-        <label className='text-xl font-semibold mr-2'>Wallet Address:</label>
-        <input type="text" className='border-2' />
-        </div>
-        <div className='m-2'>
-            <button className='bg-slate-400 text-lg p-2 rounded-lg hover:bg-slate-200'>Submit</button>
-        </div>
-    </div>
-     */}
+    
      <>
     <div className='bg-img-institute h-screen bg-cover'>
     <Navbar/>
@@ -48,7 +30,7 @@ const StudentRegister = () => {
     
     </>
     
-    </>
+    
     
   )
 }
